3b00fa28b101a3fe168fe5fb5c83c7aff9c5b37a
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 13:31:19 +0900

    Add files via upload

---
+++ Programmers/LV0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(string[] strlist)
+    {
+        int[] answer=new int[strlist.Length];
+        for (int i = 0; i < strlist.Length; i++)
+        {
+            answer[i] = strlist[i].Length;
+        }
+        return answer;
+    }
+    static void Main(string[] args)
+    {
+        string[] list = { "We", "are", "the", "world!" };
+
+    }
+}
