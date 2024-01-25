3c9051f67ccd0732bd391a98c7adaf523bb726c6
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 13:31:58 +0900

    Add files via upload

---
+++ Programmers/LV0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+public class Solution
+{
+    public static int[] solution(int[] num_list)
+    {
+        int[] answer=new int[num_list.Length];
+        Array.Reverse(num_list);
+        answer = num_list;
+        return answer;
+    }
+
+    static void Main(string[] args)
+    {
+        int[] num = { 1, 2, 3, 4, 5 };
+        
+        Console.WriteLine(solution(num));
+    }
+}
