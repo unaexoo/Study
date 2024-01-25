964fdbc269c674bb487c5d78ab0c480f0a2b5285
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 13:23:43 +0900

    Add files via upload

---
+++ Programmers/LV0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Solution
+{
+    public static string solution(string my_string)
+    {
+        string answer = "";
+        char[] strArr = my_string.ToCharArray();
+        Array.Reverse(strArr);
+        answer = string.Concat(strArr);
+        return answer;
+    }
+    static void Main(string[] args)
+    {
+        string str = "jaron";
+        solution(str);
+    }
+}
