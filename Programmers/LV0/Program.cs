a453f644c3af7b84c5926a03d3e386c7df064ef1
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 13:25:20 +0900

    Add files via upload

---
+++ Programmers/LV0/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace 문자열곱하기
+{
+
+    public class Solution
+    {
+        public string solution(string my_string, int k)
+        {
+            string answer = "";
+            for(int i=0;i<k; i++)
+            {
+                answer += my_string;
+            }
+            return answer;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution sol = new Solution();
+
+            Console.WriteLine(sol.solution("string",3));
+        }
+    }
+}
