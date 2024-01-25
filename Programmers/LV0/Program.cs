6357e84096d15d9a4e8bb9de7fe4a3dd9f6cd9f0
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 13:34:14 +0900

    Add files via upload

---
+++ Programmers/LV0/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+public class Solution
+{
+    public int[] solution(int numer1, int denom1, int numer2, int denom2)
+    {
+        int[] answer = new int[] { };
+        int numer = (numer1 * denom2) + (numer2 * denom1);
+        int denom = numer1 * numer2;
+ 
+        return answer;
+    }
+}
