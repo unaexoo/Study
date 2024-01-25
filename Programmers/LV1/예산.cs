627384f48d2eaebf95fab99080bcf40fda231899
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 13:57:33 +0900

    Create 예산.cs

---
+++ Programmers/LV1/예산.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Solution {
+    public int solution(int[] d, int budget) {
+        int answer = 0;
+            int sum = 0;
+            Array.Sort(d);
+            for(int i=0; i<d.Length; i++)
+            {
+                sum += d[i];
+                answer++;
+                if(sum > budget)
+                {
+                    answer--;
+                }
+            }
+            return answer;
+    }
+}
