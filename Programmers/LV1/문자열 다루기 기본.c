e770da044c5ee9a6a73750820bfc12ba180ae35f
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 14:04:18 +0900

    Create 문자열 다루기 기본.c

---
+++ Programmers/LV1/문자열 다루기 기본.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include<string.h>
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+bool solution(const char* s) {
+    bool answer = true;
+
+    if (strlen(s) == 4 || strlen(s) == 6) {
+        for (int i = 0; i < strlen(s); i++) {
+            if (s[i] >= '0' && s[i] <= '9') {
+                answer = true;
+            }
+            else
+            {
+                answer = false;
+                return answer;
+            }
+        }
+        return answer;
+    }
+    else
+    {
+        answer = false;
+        return answer;
+    }
+}
