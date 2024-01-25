36182e153d998ddb5d3b7f5cc8c300517a7332a0
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 14:03:47 +0900

    Create 수박수박수박수박수박수?.c

---
+++ Programmers/LV1/수박수박수박수박수박수?.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include<string.h>
+char* solution(int n) {
+    // 리턴할 값은 메모리를 동적 할당해주세요.
+  
+    char* answer = (char*)malloc((sizeof(char)*n*3)); 
+    strcpy(answer, "\0");
+
+    for (int i = 0; i <n; i++) {
+        if (i % 2 == 0) {
+            strcat(answer, "수");
+        }
+        else
+        {
+            strcat(answer, "박");
+        }
+    }
+    return answer;
+}
