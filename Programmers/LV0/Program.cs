d8ea5c8e91ae43b1aacd05ffc2c8449b5f2a70b4
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 13:34:50 +0900

    Add files via upload

---
+++ Programmers/LV0/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class Solution
+{
+    public static int solution(int[] sides)
+    {
+        int answer = 2;
+        Array.Sort(sides);
+        for (int i = 0; i < sides.Length; i++)
+        {
+            Console.WriteLine(sides[i]);
+        }
+        if (sides[0] + sides[1] > sides[2])
+        {
+            answer = 1;
+        }
+
+        return answer;
+
+    }
+    static void Main(string[] args)
+    {
+        int[] num = { 1, 2, 3 };
+        solution(num);
+    }
+}
