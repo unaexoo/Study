29dac98f988e7152e4f16508501ed7ef0eab9efe
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 13:58:40 +0900

    Create 행렬의 덧셈.cs

---
+++ Programmers/LV1/행렬의 덧셈.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int[,] solution(int[,] arr1, int[,] arr2) {
+         int[,] answer = new int[arr1.GetLength(0), arr1.GetLength(1)];
+            for(int i=0; i<answer.GetLength(0); i++)
+            {
+                for(int j=0; j<answer.GetLength(1); j++)
+                {
+                    answer[i, j] = arr1[i, j] + arr2[i, j];
+                }
+            }
+                return answer;
+        
+    }
+}
