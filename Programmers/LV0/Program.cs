2266b49757fd3f3a2a04f8716170e18e8bd274c0
윤혜주 <142863284+unaexoo@users.noreply.github.com>
2024-01-25 13:35:17 +0900

    Add files via upload

---
+++ Programmers/LV0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution
+{
+    public int solution(int n, int t)
+    {
+        int answer = n;
+        for (int i = 1; i <=t; i++)
+        {
+            answer *= 2;
+        }
+        return answer;
+    }
+    static void Main(string[] args)
+    {
+        int num = 2;
+        int t = 10;
+        Solution sol = new Solution();
+        Console.WriteLine(sol.solution(num, t));
+    }
+}
